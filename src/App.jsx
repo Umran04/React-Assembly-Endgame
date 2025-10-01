a16ecaa0cf70ae6df4e7842b0f219aad241fca06
Umran <mohammedumran.0416@gmail.com>
2025-10-01 17:15:04 +0100

    basic setup and styling header, try and use meaningful semantics

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function App(){
     return(
-    <div > 
-        <h1>Assembly Endgame</h1>
-        <p>Guess the words within 8 attempts to keep the programming languages safe from assembly</p>
-    </div>
+    <main>
+        <header>
+            <h1>Assembly Endgame</h1>
+            <p>Guess the words within 8 attempts to keep the programming languages safe from assembly</p>
+        </header>
+    </main>
     )
 }
