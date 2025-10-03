62a470f7344de9e1c26723e24990979365f99bec
Umran <mohammedumran.0416@gmail.com>
2025-10-03 11:39:45 +0100

    made functionality so that checks to see if game is over based on some conditions and renders the new game button to the page

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,28 @@ export default function App(){
     //then use .length to show how many wrong guesses there has been 
     const wrongGuessCount = userGuessLetter.filter(letter => !currentWord.includes(letter)).length
     
+    //opposite of prev line, conditions to conditionally render New Game button
+    const correctGuessCount = userGuessLetter.filter(letter => currentWord.includes(letter)).length === currentWord.length
+
+    let isGameOver = false
+    if(wrongGuessCount === 8 || correctGuessCount){
+        isGameOver = true
+    }
 
     
     
     /*LANGUAGE LIST*/
     const languageList = languages.map( (language, index) => { 
-
-    const className = clsx("chips",{ lost: index < wrongGuessCount })
-
+    
+    const isLost = index < wrongGuessCount
+    const className = clsx("chips", isLost && "lost") // another way of using clsx
+    //adds lost css class name if the index of languages is less than wrong guess count
+    
+    //HOW THIS WORKS: .map means it goes thru each element in the array
+    //say for e.g if we are at index 0 and wrong guess count is 0, that means 0 is not less than 0 so lost will NOT be added
+    //now if index is 0 and WGC is 1, since 0 < 1 lost will be applied, then it will move thru the array again
+    //1 < 1 ? no so only index 0 will have the lost applied, same goes for 2 to 8.
+    //WGC = 5, will go through array until index is at 5 etc.
 
     return <span className={className} key={language.name} 
     style={ {backgroundColor: language.backgroundColor, color:language.color}}>
@@ -36,20 +50,20 @@ export default function App(){
 
     /*KEYBOARD*/
     const keyboard = alphabet.split("").map((letter) => {
-        //cant write notes next to clsx stuff but this basically meanes, line 1 is just default keyboard--letters classname
-        //line 2 is if the letter has been guessed and is in the word then make the letter green (.correct css class)
-        //line 3 if the letter has been guessed and is not in the word then make it red (.wrong css class)
         
-        //refactor the clsx
+        
+        
+        const isCorrect = userGuessLetter.includes(letter) && currentWord.includes(letter)
+        const isWrong = userGuessLetter.includes(letter) && !currentWord.includes(letter)
+        //checking which guesses are correct and wrong
+        
+        const className = clsx("keyboard--letters", { correct : isCorrect , wrong : isWrong})
+        //base case have keyboard--letters, 
+        //based on correct or incorrect guesses apply the class name to change its style
        
         
         return <button onClick={() => userClick(letter)}
-        
-            className={clsx(
-            "keyboard--letters",
-            userGuessLetter.includes(letter) && currentWord.includes(letter) && "Correct",
-            userGuessLetter.includes(letter) && !currentWord.includes(letter) && "Wrong")}>
-            
+            className={className}>
             {letter.toUpperCase()} 
             </button>
         }
@@ -87,7 +101,7 @@ export default function App(){
 
         <section className="keyboard">{keyboard}</section>
 
-        <button className="newGame--btn">New Game</button>
+        {isGameOver && <button className="newGame--btn">New Game</button>}
     </main>
     )
 }
